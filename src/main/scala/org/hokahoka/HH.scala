0bc77f08186f65aa9a169be51e48f8fa4c13a364
Kenta Takagaki <kentaroh.takagaki@gmail.com>
2015-03-10 18:58:42 +0100

    150310

---
+++ src/main/scala/org/hokahoka/HH.scala
@@ -0,0 +1,24 @@
+package org.hokahoka
+
+/**
+ * Created by ktakagaki on 15/02/19.
+ */
+object HH {
+
+  def simplePixelCluster(list: Array[Array[Double]]): Array[Double] = {
+
+    require(list != null, "Input cannot be null!")
+
+    var match1
+
+
+    for(pixel <- list){
+
+
+    }
+
+
+  }
+
+
+}
